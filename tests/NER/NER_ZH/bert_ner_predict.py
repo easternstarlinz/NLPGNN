0256bcea163fe08d7f9328f5d792980f29593580
easternstarlinz <easternstarlinz@gmail.com>
2021-01-29 16:39:59 +0800

    Update bert_ner_predict.py

--- tests/NER/NER_ZH/bert_ner_predict.py
+++ tests/NER/NER_ZH/bert_ner_predict.py
@@ -12,7 +12,7 @@ load_check = LoadCheckpoint(language='zh')
 param, vocab_file, model_path = load_check.load_bert_param()
 
 # 定制参数
-param.batch_size = 6
+param.batch_size = 1
 param.maxlen = 128
 param.label_size = 49
 
